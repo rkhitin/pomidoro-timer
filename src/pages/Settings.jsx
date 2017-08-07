c866bd4c1c037b05e7e51157416023fe7e79b136
ronhitin <rkhitin@gmail.com>
2017-08-07 10:46:14 +0400

    Add eslint config

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,3 +1,5 @@
+// @flow
+
 import React from 'react'
 
 const Settings = ({
@@ -8,17 +10,25 @@ const Settings = ({
   actions: Object,
 }) => {
   console.dir(actions)
-  return <div>
-
+  return (
     <div>
-      Время работы
-      <hr />
-      <button onClick={actions.work.decrease}><i className="fa fa-minus"/></button>
-      <input type="text" value={settings.durations.work} onChange={actions.work.set} />
-      <button onClick={actions.work.decrease}><i className="fa fa-plus"/></button>
+      <div>
+        Время работы
+        <hr />
+        <button onClick={actions.work.decrease}>
+          <i className="fa fa-minus" />
+        </button>
+        <input
+          type="text"
+          value={settings.durations.work}
+          onChange={actions.work.set}
+        />
+        <button onClick={actions.work.decrease}>
+          <i className="fa fa-plus" />
+        </button>
+      </div>
     </div>
-
-  </div>
+  )
 }
 
 Settings.propTypes = {}
--- src/utils/common.js
+++ src/utils/common.js
@@ -1,7 +1,9 @@
 // @flow
 
 export function calculateTargetTimeInMin(settings: Settings): number {
-  let bigBreakNumbers = Math.floor(settings.targetNumberOfPomidoro / settings.pomidoroInRound)
+  let bigBreakNumbers = Math.floor(
+    settings.targetNumberOfPomidoro / settings.pomidoroInRound
+  )
 
   if (settings.targetNumberOfPomidoro % settings.pomidoroInRound === 0) {
     bigBreakNumbers--
@@ -10,9 +12,9 @@ export function calculateTargetTimeInMin(settings: Settings): number {
   return (
     bigBreakNumbers * settings.intervalDurationsInMin.bigBreak +
     settings.targetNumberOfPomidoro * settings.intervalDurationsInMin.work +
-    (settings.targetNumberOfPomidoro - 1 - bigBreakNumbers) * settings.intervalDurationsInMin.smallBreak
+    (settings.targetNumberOfPomidoro - 1 - bigBreakNumbers) *
+      settings.intervalDurationsInMin.smallBreak
   )
 }
 
 export const getMinutes = (time: number): number => Math.floor(time / 60)
-
