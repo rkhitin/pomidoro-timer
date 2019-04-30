8f02ab0a74f259e2ae948d6ae7d6281942503841
Ron Hitin <rkhitin@gmail.com>
2019-04-30 17:28:31 +0300

    Updates

---
+++ precache-manifest.2d1f737131b039432c7e7f67247e6677.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1d4febbedaabe249f03368449178a66",
+    "url": "/pomidoro-timer/index.html"
+  },
+  {
+    "revision": "56e177698d4d411ed93c",
+    "url": "/pomidoro-timer/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "778ff12ad5245156cb10",
+    "url": "/pomidoro-timer/static/css/main.ef503aa7.chunk.css"
+  },
+  {
+    "revision": "56e177698d4d411ed93c",
+    "url": "/pomidoro-timer/static/js/2.d53c6920.chunk.js"
+  },
+  {
+    "revision": "778ff12ad5245156cb10",
+    "url": "/pomidoro-timer/static/js/main.f99a929e.chunk.js"
+  },
+  {
+    "revision": "066f42b0611cdecbfbdf",
+    "url": "/pomidoro-timer/static/js/runtime~main.9e8e3878.js"
+  }
+]);
